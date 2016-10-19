050c0b003146210fb58877278a44e0eec244bd30
benjadahl <benjadahl@gmail.com>
2016-10-20 00:37:21 +0200

    Fixed multiple expressions in the same tag

--- main.js
+++ main.js
@@ -15,8 +15,15 @@ function renderPreview(){
   //If there is take the one at position 0 and replace it. This works because the list is live updating.
   while(preTags.length > 0){
     var element = preTags[0];
-    userExpressions.push(new algebra.parse(element.innerHTML));
-    $(element).replaceWith(katex.renderToString(math.parse(element.innerHTML).toTex() + "=" + userExpressions[userExpressions.length - 1]));
+    var replaceString = "";
+    var mathArray = element.innerHTML.split("\n");
+    for (m of mathArray){
+      if(m !== ""){
+        userExpressions.push(new algebra.parse(m));
+        replaceString += katex.renderToString(math.parse(m).toTex() + "=" + userExpressions[userExpressions.length - 1]) + "<br>";
+      }
+    }
+    $(element).replaceWith(replaceString);
   }
   $("#preview").append(data.getElementsByTagName("body")[0].innerHTML);
 }
