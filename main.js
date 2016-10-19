b8d316edc5c93fe2941a5f9e8fdc4ef67de7aa2f
benjadahl <benjadahl@gmail.com>
2016-10-19 22:25:40 +0200

    Made the code a lot nicer

--- main.js
+++ main.js
@@ -2,55 +2,21 @@ CKEDITOR.replace("editor");
 
 CKEDITOR.instances.editor.setData("<h1>Testman</h1>");
 
-var z = new Expression("z");
-
-var eq1 = new Equation(z.multiply(2), z.pow(3));
+parser = new DOMParser();
 
 var userExpressions = [];
-
-console.log(eq1.toString());
-console.log(eq1.solveFor("z").toString());
 
 function renderPreview(){
   $("#preview").empty();
   var eData = CKEDITOR.instances.editor.getData();
-  var readMath = false;
-  var math = "";
-  var previewHtml = "";
-
-  for (i=0; i < eData.length; i++){
-    //Check if the character should pre recorded as math
-    if(readMath){
-      math += eData[i];
-    }else{
-      previewHtml += eData[i];
-    }
-
-    //Check if the character is the end of a pre tag
-    if(eData[i] === ">" && eData[i-1] === "e"){
-      var next = eData[i-2] + eData[i-3] + eData[i-4];
-      if(next === "rp<"){
-        math = "";
-        readMath = true;
-      }
-    }
-
-    //Check if the character is the beginning of a pre end tag
-    if(eData[i+1] === "<" && eData[i+2] === "/"){
-      var next = eData[i+3] + eData[i+4] + eData[i+5] + eData[i+6];
-      if(next === "pre>"){
-        userExpressions.push(new algebra.parse(math.substring(1)));
-        try{
-          previewHtml += katex.renderToString(math + "=" + userExpressions[userExpressions.length - 1]);
-        }catch(err){
-          //Do nothing
-        }
-        readMath = false;
-      }
-    }
+  var data = parser.parseFromString(eData, "text/html");
+  var preTags = data.getElementsByTagName("pre");
+  console.log(data);
+  for (T of preTags){
+    userExpressions.push(new algebra.parse(T.innerHTML));
+    $(T).replaceWith(katex.renderToString(math.parse(T.innerHTML).toTex() + "=" + userExpressions[userExpressions.length - 1]));
   }
-  userExpressions = [];
-  $("#preview").append(previewHtml);
+  $("#preview").append(data.getElementsByTagName("body")[0].innerHTML);
 }
 
 setInterval(function() {
