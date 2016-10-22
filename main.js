27875432a9fda0838164addf66b434f56f6b40b8
benjadahl <benjadahl@gmail.com>
2016-10-22 23:20:21 +0200

    Misc. improvements

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ var userExpressions = [];
 
 function solve (eq, variable) {
   eq = new algebra.parse(eq);
-  return eq.solveFor(variable).toString();
+  solved = eq.solveFor(variable)
+  console.log(eq.solveFor(variable).toString());
+  return {string: solved.toString(), algebra: solved};
 }
 
 function renderPreview(){
@@ -25,24 +27,25 @@ function renderPreview(){
     for (m of mathArray){
       if(m !== ""){
         var curlyOpen = m.indexOf("{");
+        var curlyClose = m.indexOf("}");
         if(curlyOpen !== -1){
-          var curlyClose = m.indexOf("}");
           if(curlyClose !== -1){
             var args = m.substring(curlyOpen + 1 , curlyClose);
             args = args.split(",");
             var ls = args[1];
             var rs = window[m.substring(0, curlyOpen)](args[0], args[1]);
-            userExpressions.push({"ls": ls, "rs": rs});
+            userExpressions.push({ls: ls, rs: rs});
           }
         }else{
-          userExpressions.push({"ls": m, "rs": new algebra.parse(m).toString()});
+          userExpressions.push({ls: m, rs: {string: new algebra.parse(m).toString()}});
         }
-        replaceString += katex.renderToString(math.parse(userExpressions[userExpressions.length - 1].ls).toTex() + "=" + math.parse(userExpressions[userExpressions.length - 1].rs).toTex()) + "<br>";
-      }
+        console.log(userExpressions);
+        replaceString += katex.renderToString(math.parse(userExpressions[userExpressions.length - 1].ls).toTex() + "=" + math.parse(userExpressions[userExpressions.length - 1].rs.string).toTex()) + "<br>";      }
     }
     $(element).replaceWith(replaceString);
   }
   $("#preview").append(data.getElementsByTagName("body")[0].innerHTML);
+  userExpressions = [];
 }
 
 setInterval(function() {
